dc24db40bdc9b86d1df46c8c485eabd885273f34
maxime-baron <maxime.baron2002@mail.com>
2024-11-12 16:39:01 +0100

    :wheelchair: Fix JBH-20 return specific error if username is already used

--- src/components/auth/actions.ts
+++ src/components/auth/actions.ts
@@ -39,6 +39,9 @@ export async function login(prevState: AuthState | undefined, formData: FormData
 
     if (error) {
         console.error(error)
+        if(error.code === 'email_not_confirmed'){
+            return { message: error.message + ', please check your emails' }
+        }
         return { message: error.message }
     }
 
@@ -60,10 +63,17 @@ export async function register(prevState: AuthState | undefined, formData: FormD
         return { errors: parse.error.flatten().fieldErrors }
     }
 
-    const { username, ...data } = parse.data
+    const { username, ...fields } = parse.data
     const avatar_url = `https://api.dicebear.com/9.x/adventurer/png?seed=${username}&radius=50&backgroundColor=b6e3f4,c0aede,d1d4f9&size=128`
 
-    const { data: { user } } = await supabase.auth.signUp({ ...data, options: { data: { username, avatar_url } } })
+    /* Check if the username is already used or not (we have exception in db function for this, but it doesn't return the error to front ) */
+    const { data } = await supabase.from('profiles').select('username').eq('username', username).single()
+    console.info(data)
+    if(data){
+        return { message: 'Username already in use' }
+    }
+
+    const { data: { user, session } } = await supabase.auth.signUp({ ...fields, options: { data: { username, avatar_url } } })
 
     /** Signup with currently existing email give a fake user without role
      * So we check the role to check if the user already exist or not
@@ -72,6 +82,10 @@ export async function register(prevState: AuthState | undefined, formData: FormD
         return { message: 'Email already exist' }
     }
 
+    if(session){
+        revalidatePath('/', 'layout')
+        redirect('/')
+    }
     revalidatePath('/', 'layout')
-    redirect('/')
+    redirect('/auth/login')
 }
