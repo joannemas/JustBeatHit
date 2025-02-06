f750f90800e76a1a9f1bb8139a9710f4a15c5095
Joanne <90553540+joannemas@users.noreply.github.com>
2025-02-06 22:17:10 +0100

    💄 Refonte auth

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -11,8 +11,8 @@ export default function Page() {
   return (
     <div className="form-container">
       <div className='form-header'>
-        <h3>Register</h3>
-        <p className="description">Provide email and password to create an account</p>
+        <h3>Inscription</h3>
+        <p className="description">Créez un compte avec un email et un mot de passe.</p>
       </div>
       <form action={formAction} aria-describedby="form-error">
         {/* Email field */}
@@ -20,22 +20,22 @@ export default function Page() {
         <input type="text" id="email" name="email" placeholder="exemple@email.com" aria-describedby="email-error" />
         <span id="email-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.email ? state.errors?.email[0] : " "}</span>
         {/* Username field */}
-        <label htmlFor="email">Username</label>
-        <input type="text" id="username" name="username" placeholder="JohnDoe" aria-describedby="email-error" />
-        <span id="email-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.username ? state.errors?.username[0] : " "}</span>
+        <label htmlFor="username">Nom d&apos;utilisateur</label>
+        <input type="text" id="username" name="username" placeholder="JohnDoe" aria-describedby="username-error" />
+        <span id="username-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.username ? state.errors?.username[0] : " "}</span>
         {/* Password field */}
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" placeholder="Create password" aria-describedby="password-error" />
+        <label htmlFor="password">Mot de passe</label>
+        <input type="password" id="password" name="password" placeholder="Créez un mot de passe" aria-describedby="password-error" />
         <span id="password-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.password ? state.errors?.password[0] : " "}</span>
         {/* Repeat password field */}
-        <label htmlFor="repeat-Password">Repeat password</label>
-        <input type="password" id="repeat-password" name="repeatPassword" placeholder="Repeat your password" aria-describedby="repeatpassword-error" />
-        <span id="repeatPassword-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.repeatPassword ? state.errors?.repeatPassword[0] : " "}</span>
+        <label htmlFor="repeat-password">Confirmez le mot de passe</label>
+        <input type="password" id="repeat-password" name="repeatPassword" placeholder="Confirmez votre mot de passe" aria-describedby="repeatpassword-error" />
+        <span id="repeatpassword-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.errors?.repeatPassword ? state.errors?.repeatPassword[0] : " "}</span>
 
         <span id="form-error" className="form-error" aria-live="polite" aria-atomic="true">{state?.message}</span>
         <SignUpButton />
       </form>
-      <Link href="/auth/login" className="link">Already have an account? Sign in here.</Link>
+      <Link href="/auth/login" className="link">Vous avez déjà un compte ? Connectez-vous ici.</Link>
     </div>
   )
 }
@@ -46,8 +46,7 @@ function SignUpButton() {
   return (
     <button type="submit" disabled={pending} aria-disabled={pending}>
       {pending && <Loader2 className='spinner' />}
-      {!pending && "Register"}
+      {!pending && "S'inscrire"}
     </button>
   )
 }
-
