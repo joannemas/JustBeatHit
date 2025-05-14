39c9ab4d4839bdabaa1d3023f2c7f1b8dbb9d24c
Joanne <90553540+joannemas@users.noreply.github.com>
2025-05-14 22:38:02 +0200

    🚧 Adapt score page

--- src/components/GameResult/GameResult.tsx
+++ src/components/GameResult/GameResult.tsx
@@ -3,33 +3,89 @@ import styles from '@/stylesheets/gameend.module.scss';
 import ShareButton from './ShareButton';
 import { createClient } from '@/lib/supabase/server';
 import ReplayButton from './ReplayButton';
+import Image from 'next/image';
 
 export default async function GameResult({ gameId }: { gameId: string }) {
     const supabase = createClient()
     const user = (await supabase.auth.getUser()).data.user ?? undefined
 
-    const gameData = (await supabase.from('games').select().eq('id', gameId).single()).data ?? undefined
+    const gameData = (await supabase
+        .from('games')
+        .select('*, song:song(title, singer)')
+        .eq('id', gameId)
+        .single()).data ?? undefined;
+
 
     return (
-        <div className={styles.finalScore}>
-            <div className={styles.scoreDisplay}>
-                <p>Score final: {gameData?.score}</p>
-                {/* <p>Nombre de lignes en pause : {pauseCount} pauses / {totalLines} lignes</p> */}
-                <p>Vitesse de frappe : {gameData?.word_speed} mots par minute</p>
-                <p>Précision d&apos;écriture : {gameData?.typing_accuracy}%</p>
-                {/* <p>Nombre de fautes : {data.mistakes} / {totalChars}</p> */}
-            </div>
-            <div className={styles.btnList}>
-                {/* If the user is not the game owner, render challenge button */}
-                {user?.id !== gameData?.user_id && <ReplayButton gameId={gameId}>Défié</ReplayButton>}
+        <div className={styles.gameResult}>
+            <div className={styles.animatedBackground}></div>
 
-                {/* If the user is the game owner, render differents buttons */}
-                {user?.id === gameData?.user_id && <ReplayButton gameId={gameId}>Rejouer</ReplayButton>}
-                {user?.id === gameData?.user_id && <Link href="/game/karakaku">
-                    <button className={styles.btnSecondary}>Retour choix de musiques</button>
-                </Link>}
-                {user?.id === gameData?.user_id && <ShareButton score={gameData?.score} />}
+            <div className={styles.gameInfos}>
+                <Image priority
+                    src="/assets/img/logo-jbh.png"
+                    alt="Logo Just Beat Hit"
+                    width={100}
+                    height={100}
+                    className={styles.logoJbh}
+                />
+
+                <div className={styles.titleSong}>
+                    <Image
+                        src="/assets/img/icon/down-round-arrow.svg"
+                        alt="Arrow svg"
+                        width={30}
+                        height={30}
+                        className={styles.musicIcon}
+                    />
+                    <h5>{gameData?.song?.singer} - {gameData?.song?.title}</h5>
+                </div>
+
+                <Image priority
+                    src="/assets/img/vinyl-jbh.svg"
+                    alt="Vinyl svg"
+                    width={1000}
+                    height={1000}
+                    className={`${styles.vinylPlayer} ${styles['--playing']}`}
+                />
+            </div>
+
+            <div className={styles.finalScore}>
+                <h2>Classement</h2>
+                <div className={styles.scoreDisplay}>
+                    {/* <p>Nombre de lignes en pause : {pauseCount} pauses / {totalLines} lignes</p> */}
+                    <p>Vitesse de frappe : {gameData?.word_speed} mots par minute</p>
+                    <p>Précision d&apos;écriture : {gameData?.typing_accuracy}%</p>
+                    {/* <p>Nombre de fautes : {data.mistakes} / {totalChars}</p> */}
+                </div>
+
+                <div className={styles.score}>
+                <div className={styles.score_display}>
+                <div className={styles.scoreLine}>
+                <Image src="/assets/img/icon/score-line.svg" alt="Score" width={24} height={24} />
+                    <p className={styles.actualScore}>{gameData?.score}</p>
+                </div>
+                    <p className={styles.label}>Score</p>
+                    </div>
+                </div>
+
+
+                <h3>Bien joué !</h3>
+                <div className={styles.btnList}>
+                    {/* If the user is not the game owner, render challenge button */}
+                    {user?.id !== gameData?.user_id && <ReplayButton gameId={gameId}>Défié</ReplayButton>}
+
+                    {/* If the user is the game owner, render differents buttons */}
+                    {user?.id === gameData?.user_id && <ReplayButton gameId={gameId}>Recommencer</ReplayButton>}
+                    {user?.id === gameData?.user_id && <Link href="/game/karakaku">
+                        <button className={styles.btnEchap}>Choix des musiques</button>
+                    </Link>}
+                    {user?.id === gameData?.user_id && <ShareButton score={gameData?.score} />}
+                </div>
+
+                <div className={`${styles.animatedBackground} ${styles['--inverse']}`}></div>
+
             </div>
         </div>
+        
     );
 }
