7be2d5491db0e469366e036e61479768096d0af5
Joanne <90553540+joannemas@users.noreply.github.com>
2025-05-15 21:10:58 +0200

    ✨ Change navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,53 +1,32 @@
-import Link from 'next/link';
-import styles from '@/stylesheets/navbar.module.scss';
-import Image from "next/image";
-import { createClient } from "@/lib/supabase/server";
+import NavLink from './NavLink'
+import styles from '@/stylesheets/navbar.module.scss'
+import Image from 'next/image'
 
-export default async function Navbar() {
-    const supabase = createClient()
-
-    const { data: { user } } = await supabase.auth.getUser()
-    console.log('user', user)
-    const { data } = await supabase.from('profiles').select('*').eq('id', user?.id!).single()
-    console.log('data', data)
-
+export default function Navbar() {
     return (
         <div className={styles.navbar}>
-            <ul><li><Link href="/">
-                <Image
-                    priority
-                    src="/assets/img/LogoMini.svg"
-                    alt="Logo Mini"
-                    className={styles.LogoMini}
-                    width={57}
-                    height={57}
-                />
-
-            </Link></li></ul>
-            <ul>
-                <li>Jeux</li>
-                <li>l&apos;équipe JBH</li>
-                <li>Contact</li>
-                <li>à propos</li>
-            </ul>
-            {data ?
-                <ul>
-                    <li>Comment ça va <span>{data.username}</span> ?</li>
-                    <a href={`/profile/${data.username}`}>
-                        <Image
-                            priority
-                            src={data.avatar_url}
-                            alt="Profil"
-                            width={44}
-                            height={44}
-                        />
-                    </a>
-                </ul>
-                :
-                <ul>
-                    <li>Bienvenue, <a href={`/auth/register`} className={styles.registerLink}>inscris toi</a> pour jouer ! </li>
-                </ul>
-            }
+        <div className={styles.logoNavbar}>
+            <Image
+            priority
+            src="/assets/img/logo-jbh.png"
+            alt="Logo Just Beat Hit"
+            width={100}
+            height={100}
+            className={styles.logoNavbarJBH}
+            />
         </div>
-    );
+
+        <ul>
+            <NavLink href="/">Accueil</NavLink>
+            <NavLink href="/game/karakaku">Karakaku</NavLink>
+            <NavLink href="/game/paroles-en-tete">Paroles en tête</NavLink>
+            <NavLink href="/game/blind-test">Blind test</NavLink>
+            <NavLink href="/profil">Profil</NavLink>
+            <NavLink href="/options">Options</NavLink>
+            <NavLink href="/auth/logout">Déconnexion</NavLink>
+        </ul>
+
+        <div className={styles.decorationNavbar}></div>
+        </div>
+    )
 }
