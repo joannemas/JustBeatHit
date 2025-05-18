a1f72153b16f164973451b323123f3c1c1830d0a
maxime-baron <maxime.baron2002@mail.com>
2025-05-18 12:17:30 +0200

    :sparkless: Add leader board

--- src/components/GameResult/GameResult.tsx
+++ src/components/GameResult/GameResult.tsx
@@ -8,13 +8,33 @@ import Image from 'next/image';
 export default async function GameResult({ gameId }: { gameId: string }) {
     const supabase = createClient()
     const user = (await supabase.auth.getUser()).data.user ?? undefined
+    /** @todo: Improve Error handling */
 
     const gameData = (await supabase
         .from('games')
         .select('*, song:song(title, singer)')
         .eq('id', gameId)
         .single()).data ?? undefined;
+    
+    if (!gameData) {
+        return <div>Game not found</div>;
+    }
+    console.log('gameData', gameData)
 
+    /** @todo: Ajoute le classement de la personne qui à fais al partie */
+
+    // Fetch the top 3 scores with usernames
+    const gameLeaderBoard = (await supabase
+        .from('best_score')
+        .select('score, profiles(username, avatar_url, user_id)') // Jointure pour obtenir le username et l'avatar
+        .eq('song_id', gameData?.song_id!)
+        .eq('game', gameData?.game_name)
+        .order('score', { ascending: false }) // Trier en fonction du premier qui a eu le score
+        .order('created_at', { ascending: true }) // Trier en fonction de la date de création
+        .limit(3)
+    ).data ?? undefined;
+
+    console.log('gameLeaderBoard', gameLeaderBoard)
 
     return (
         <div className={styles.gameResult}>
@@ -51,6 +71,27 @@ export default async function GameResult({ gameId }: { gameId: string }) {
 
             <div className={styles.finalScore}>
                 <h2>Classement</h2>
+                <ul className={styles.leaderBord}>
+                    {(gameLeaderBoard && gameLeaderBoard.length > 0) && 
+                        gameLeaderBoard.map((item, index) => (
+                            <li key={index} className={styles.leaderBoardItem}>
+                                <div className={styles.inner}>
+                                    <div className={styles.imageWrapper}>
+                                        <Image
+                                            src={item.profiles?.avatar_url || ''}
+                                            alt="User avatar"
+                                            width={64}
+                                            height={64}
+                                            className={styles.itemAvatar}
+                                        />
+                                    </div>
+                                    <p className={styles.itemUsername}>{index + 1} - {item.profiles?.username}</p>
+                                    <p className={styles.itemScore}>{item.score} points</p>
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
                 <div className={styles.scoreDisplay}>
                     {/* <p>Nombre de lignes en pause : {pauseCount} pauses / {totalLines} lignes</p> */}
                     <p>Vitesse de frappe : {gameData?.word_speed} mots par minute</p>
