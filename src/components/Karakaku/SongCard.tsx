defcba1d28d09ea4a308cd358e94e1d6624cb87a
Melvin CHEVALLIER <mchevallier@aubay.com>
2025-07-17 14:46:34 +0200

    fix image

--- src/components/Karakaku/SongCard.tsx
+++ src/components/Karakaku/SongCard.tsx
@@ -16,29 +16,28 @@ export default function SongCard({ song, gameId }: { song: Database["public"]["T
     return (
         <Link key={song.id} onClick={handleClick} href={`game/karakaku/${gameId}`} className={styles.songCard}>
             <div className={styles.imageBack}>
-                <img
-                src="https://plus.unsplash.com/premium_photo-1682096297493-96a4ac18dd4d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0"
+              <img
+                src={`https://fyuftckbjismoywarotn.supabase.co/storage/v1/object/public/song/${encodeURIComponent(`${song.singer} - ${song.title.replace(/'/g, "")}`)}/cover.jpg`}
                 alt={song.title}
-                />
-                <div className={styles.overlay}>
-                    <div className={styles.musicStyle}>
-                        {song.music_style?.map((style, index) => (
-                            <div key={index} className={styles.musicStyleItem}>
-                            {style}
-                            </div>
-                        ))}
+              />
+              <div className={styles.overlay}>
+                <div className={styles.musicStyle}>
+                  {song.music_style?.map((style, index) => (
+                    <div key={index} className={styles.musicStyleItem}>
+                      {style}
                     </div>
-                    <div className={styles.songContentText}>
-                        <div className={styles.SingerAndTitle}>
-                            <h2 className={styles.songTitle}>{song.title}</h2>
-                            <p className={styles.songSinger}>{song.singer}</p>
-                        </div>
-                        <div className={`${styles.difficulty} ${styles[song.difficulty?.toLowerCase() || 'unknown']}`}>
-                        <p>{song.difficulty}</p>
-                        </div>
-                    </div>
-
+                  ))}
                 </div>
+                <div className={styles.songContentText}>
+                  <div className={styles.SingerAndTitle}>
+                    <h2 className={styles.songTitle}>{song.title}</h2>
+                    <p className={styles.songSinger}>{song.singer}</p>
+                  </div>
+                  <div className={`${styles.difficulty} ${styles[song.difficulty?.toLowerCase() || 'unknown']}`}>
+                    <p>{song.difficulty}</p>
+                  </div>
+                </div>
+              </div>
             </div>
         </Link>
         )
